28622c45f5404cbd40778e31fb278911a47160f4
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-03-23 04:40:58 +0800

    test: run @authing/mfa-component-react example for version 1.0.0-alpha.3 :rocket:

---
+++ examples/mfa-component-react/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { AuthingMFAProvider } from '@authing/mfa-component-react'
+
+import '@authing/mfa-component-react/dist/index.min.css'
+
+import RouterComponent from './router'
+
+import './App.css'
+
+export default function App() {
+  return (
+    <AuthingMFAProvider
+      appId="630ed3137dd6f2fd7001da24"
+    >
+      <RouterComponent></RouterComponent>
+    </AuthingMFAProvider>
+  )
+}
