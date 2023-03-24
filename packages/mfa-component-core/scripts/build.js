2f5ecc5f5865a59b1a949f81f3734d17aba81df3
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-03-25 00:35:54 +0800

    fix bug

--- packages/mfa-component-core/scripts/build.js
+++ packages/mfa-component-core/scripts/build.js
@@ -14,22 +14,21 @@ try {
 readyGo()
 
 function readyGo () {
-  webpack(webpackReact16ESMConfig, (error, stats) => {
-    console.log(stats)
+  webpack(webpackReact16ESMConfig, (error) => {
     if (error) {
       console.error('build Authing MFA core esm 16 bundler error: ', error)
     }
   })
 
-  // webpack(webpackReact18ESMConfig, (error) => {
-  //   if (error) {
-  //     console.error('build Authing MFA core esm 18 bundler error: ', error)
-  //   }
-  // })
+  webpack(webpackReact18ESMConfig, (error) => {
+    if (error) {
+      console.error('build Authing MFA core esm 18 bundler error: ', error)
+    }
+  })
 
-  // webpack(webpackGlobalConfig, (error) => {
-  //   if (error) {
-  //     console.error('build Authing MFA core global bundler error: ', error)
-  //   }
-  // })
+  webpack(webpackGlobalConfig, (error) => {
+    if (error) {
+      console.error('build Authing MFA core global bundler error: ', error)
+    }
+  })
 }
